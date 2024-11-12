2d661fdf8f97128e39287a7e0c80de854ca8dfc4
Kaan <kaan.sahin2@hotmail.com>
2024-11-12 16:26:45 +0300

    ProductController.java created

---
+++ src/main/java/com/enoca/javachallenge/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.enoca.javachallenge.controller;
+
+import com.enoca.javachallenge.model.Product;
+import com.enoca.javachallenge.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public String createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+    @GetMapping("/{productId}")
+    public Product getProduct(@PathVariable Long productId){
+        return productService.getProduct(productId);
+    }
+
+    @PutMapping("/{productId}")
+    public Product updateProduct(@RequestBody Product updatedProduct, @PathVariable Long productId){
+        return productService.updateProduct(updatedProduct ,productId);
+    }
+
+    @DeleteMapping("/{productId}")
+    public void deleteProduct(@PathVariable Long productId){
+        productService.deleteProduct(productId);
+    }
+}
