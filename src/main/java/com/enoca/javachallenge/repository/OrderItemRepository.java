d5940771e5977888e57d03c604c95dff6a54e08e
Kaan <kaan.sahin2@hotmail.com>
2024-11-12 14:49:51 +0300

    fixup! OrderItemRepository.java

--- src/main/java/com/enoca/javachallenge/repository/OrderItemRepository.java
+++ src/main/java/com/enoca/javachallenge/repository/OrderItemRepository.java
@@ -5,8 +5,4 @@ import com.enoca.javachallenge.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<Customer, Long> {
-    Order getOrder(Long orderId);
-    String createOrder(Order order);
-    Order updateOrder(Order order, Long orderId);
-    String deleteOrder(Long orderId);
 }
