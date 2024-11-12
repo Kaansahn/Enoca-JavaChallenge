3a879c6b43ffeb1b6f1453e14cdf2fe583c18905
Kaan <kaan.sahin2@hotmail.com>
2024-11-12 15:19:00 +0300

    ProductService.java implementations

--- src/main/java/com/enoca/javachallenge/service/ProductService.java
+++ src/main/java/com/enoca/javachallenge/service/ProductService.java
@@ -1,25 +1,76 @@
 package com.enoca.javachallenge.service;
 
 import com.enoca.javachallenge.model.Product;
+import com.enoca.javachallenge.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService implements IProductService{
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product getProduct(Long productId) {
-        return null;
+        List<Product> products = productRepository.findAll();
+
+        Optional<Product> optionalProduct = products.stream()
+                .filter(p -> p.getId().equals(productId))
+                .findFirst();
+
+        if(optionalProduct.isPresent()){
+            return optionalProduct.get();
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        }
     }
 
     @Override
     public String createProduct(Product product) {
-        return null;
+        productRepository.save(product);
+        return "Product successfully created!";
     }
 
     @Override
     public Product updateProduct(Product product, Long productId) {
-        return null;
+        List<Product> products = productRepository.findAll();
+
+        Optional<Product> optionalProduct = products.stream()
+                .filter(p -> p.getId().equals(productId))
+                .findFirst();
+
+        if(optionalProduct.isPresent()){
+            Product existingProduct = optionalProduct.get();
+            existingProduct.setName(product.getName());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setStock(product.getStock());
+
+            Product savedProduct = productRepository.save(existingProduct);
+
+            return savedProduct;
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        }
     }
 
     @Override
     public String deleteProduct(Long productId) {
-        return null;
+        List<Product> products = productRepository.findAll();
+
+        Product product = products.stream()
+                .filter(p -> p.getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        productRepository.delete(product);
+
+        return "Product with product id: " + productId + " deleted successfully!";
     }
 }
